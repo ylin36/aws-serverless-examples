168a68e5bfa02e71225db96a0004031e964f3812
Yang Lin <ylin366@gmail.com>
2023-06-25 16:02:54 -0400

    added initial sns example

--- aws_serverless_examples/__main__.py
+++ aws_serverless_examples/__main__.py
@@ -1,10 +1,16 @@
 # for any files sibling to this file, import relative to the module, 
 # because we're going to be installing this main module.
 from aws_serverless_examples.sns import SNSPublisher
+import boto3
 
 def main():
-    publisher = SNSPublisher()
-    publisher.print()
+    client = boto3.client('sns')
+    publisher = SNSPublisher(client)
+    response = publisher.create_topic("yang_test")
+    topic_arn = response['TopicArn'] if response else None
+    if (topic_arn):
+        response = publisher.publish_to_topic(topic_arn, "test subject", "test message")
+        print(response)
 
 if __name__ == "__main__":
     main()
--- aws_serverless_examples/sns/sns_publisher.py
+++ aws_serverless_examples/sns/sns_publisher.py
@@ -1,9 +1,54 @@
 import boto3
 
 class SNSPublisher:
-    def __init__(self):
-        pass
+    def __init__(self, sns_client):
+        self.__sns_client = sns_client
+    
+    def create_topic(self, topic_name: str) -> dict:
+        """
+            Create topic if it doesn't exist.
 
-    def print(self):
-        print("hello")
+            Parameters:
+                    topic_name (str): Name of the topic
+
+            Returns:
+                    response: {
+                        'TopicArn': 'string'
+                    }
+        """
+        return self.__sns_client.create_topic(
+            Name = topic_name,
+            Attributes = {
+                'DisplayName': 'aws_serverless_example'
+            },
+            Tags = [
+                {
+                    'Key': 'created_by',
+                    'Value': 'sns_publisher'
+                },
+            ]
+        )
+    
+    def publish_to_topic(self, topic_arn: str, subject: str, message: str) -> dict:
+        """
+            Publish to  topic if it doesn't exist.
+
+            Parameters:
+                    topic_arn (str): arn of the topic
+                    subject (str): subject
+                    message (str): Payload message
+
+            Returns:
+                    Response: {
+                        'MessageId': 'string',
+                        'SequenceNumber': 'string'
+                    }
+        """
+        return self.__sns_client.publish(
+            TopicArn = topic_arn,
+            Message = subject,
+            Subject = message,
+        )
+
+
         
